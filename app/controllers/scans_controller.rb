4570e163e0a34ca1eb90c7deadc5104b16fa80d2
Johnermac <ermac.rodrigues@protonmail.com>
2025-03-09 19:32:23 -0300

    subdomain added without logic yet

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -4,22 +4,25 @@ class ScansController < ApplicationController
   def create
     site = params[:site]
     scan_directories = params[:scan_directories] || false  # Default to false if not provided
+    scan_subdomains = params[:scan_subdomains] || false
 
-    puts "\n => Received scan_directories: #{scan_directories}" # Debug statement
+    puts "\n => Received scan_directories: #{scan_directories}" # Debug
+    puts "\n => Received scan_subdomains: #{scan_subdomains}" # Debug
+
 
     if site.blank?
       render json: { error: "Site URL is required" }, status: :unprocessable_entity
       return
     end
 
-    ScanWorker.perform_async(site, scan_directories)
+    ScanWorker.perform_async(site, scan_directories, scan_subdomains)
 
-    render json: { message: "Scan started for #{site}", scan_directories: scan_directories }, status: :accepted
+    render json: { message: "Scan started for #{site}", scan_directories: scan_directories, scan_subdomains: scan_subdomains }, status: :accepted
   end
 
   def show
     site = params[:site]
-    scan_results_json = REDIS.get("scan_results_#{site}")
+    scan_results_json = REDIS.get("scan_results_#{site}_directories")
 
     if scan_results_json.nil?
       render json: { error: "No results found for #{site}" }, status: :not_found
--- app/worker/scan_worker.rb
+++ app/worker/scan_worker.rb
@@ -6,7 +6,7 @@ class ScanWorker
   include HTTParty
   include Sidekiq::Worker
 
-  def perform(site, scan_directories)
+  def perform(site, scan_directories, scan_subdomains)
 
     # -----------------  VALIDATE WORDLIST  ---------------------
  
@@ -19,11 +19,10 @@ class ScanWorker
 
     # ---------------------  DIR  ------------------------------
 
-    directories = File.readlines(wordlist_path).map(&:strip).reject(&:empty?)
-
-    
+    directories = File.readlines(wordlist_path).map(&:strip).reject(&:empty?)    
 
     puts "\n => RUN DIR? #{scan_directories}"
+    puts "\n => RUN SUB? #{scan_subdomains}"
 
     if scan_directories
       found_directories, not_found_directories = run_directories(site, directories)
@@ -33,9 +32,8 @@ class ScanWorker
       not_found_directories: not_found_directories
       }
 
-      # Save the results to Redis (assuming you have Redis set up)
-      REDIS.set("scan_results_#{site}", result.to_json)
-      REDIS.expire("scan_results_#{site}", 10)
+      REDIS.set("scan_results_#{site}_directories", result.to_json)
+      REDIS.expire("scan_results_#{site}_directories", 10)
 
       puts "\n  Scan Results for #{site}:"
       puts "    \t✅ Found Directories: #{found_directories.join(', ')}" if found_directories.any?
@@ -43,7 +41,27 @@ class ScanWorker
 
     end   
 
-    # -------------------------------------------------------------------
+    # ---------------------  SUB  ------------------------------
+
+    subdomains = ""
+
+    if scan_subdomains
+      found_subdomains, active_subdomains = run_subdomains(site, subdomains)
+
+      result = {
+        found_subdomains: found_subdomains,
+        active_subdomains: active_subdomains 
+      }
+
+      REDIS.set("scan_results_#{site}_subdomains", result.to_json)
+      REDIS.expire("scan_results_#{site}_subdomains", 10)
+
+      puts "\n  Scan Results for #{site}:"
+      puts "    \t✅ Found Subdomains: #{found_subdomains.join(', ')}" if found_subdomains.any?
+      puts "    \t✅✅ Active Subdomains: #{active_subdomains .join(', ')}\n" if active_subdomains .any?
+
+    end
+
   end
 
   private
@@ -63,5 +81,16 @@ class ScanWorker
       end      
     end
     [found_directories, not_found_directories]
+  end
+
+
+  def run_subdomains(site, subdomains)
+    found_subdomains = []
+    active_subdomains = []
+
+    # implement logic
+    puts "\n=> Time to shine"
+
+    [found_subdomains, active_subdomains]
   end
 end
