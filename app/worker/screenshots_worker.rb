a4805bccb8a80e9c4ce74005a3769320391d9708
Johnermac <ermac.rodrigues@protonmail.com>
2025-05-03 11:03:03 -0300

    front header

--- app/worker/screenshots_worker.rb
+++ app/worker/screenshots_worker.rb
@@ -20,7 +20,7 @@ class ScreenshotsWorker
           '--disable-blink-features=AutomationControlled',
           '--disable-dev-shm-usage',
           '--ignore-certificate-errors',
-          '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36'
+          '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
         ]
       }
 
